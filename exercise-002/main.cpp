894e0803a154409073a6492d277084577da9fb22
TimoAu <156666237+TimoAu@users.noreply.github.com>
2024-10-25 09:54:26 +0200

    create directory for exercise-002

---
+++ exercise-002/main.cpp
@@ -0,0 +1,38 @@
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+#include<iostream>    
+#include<vector> 
+#include<algorithm>
+#include "CLI/CLI.hpp"
+#include "config.h.in"
+
+auto main(int argc, char **argv) -> int
+{
+    /**
+     * CLI11 is a command line parser to add command line options
+     * More info at https://github.com/CLIUtils/CLI11#usage
+     */
+    CLI::App app{PROJECT_NAME};
+    
+    int count = 20;
+    try
+    {
+        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Insert Count");        //add option to change count in Terminal
+        app.parse(argc, argv);
+    }
+    catch (const CLI::ParseError &e)
+    {
+        return app.exit(e);
+    }
+
+    /**
+     * The {fmt} lib is a cross platform library for printing and formatting text
+     * it is much more convenient than std::cout and printf
+     * More info at https://fmt.dev/latest/api.html
+     */
+    fmt::println("Hello Exercise number two");
+
+    return 0; /* exit gracefully*/
+}
+
